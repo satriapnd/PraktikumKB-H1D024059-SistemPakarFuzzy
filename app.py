f937606236b50f357e269c6385734b65ace1b7dd
satriapnd <satriapinandita17@gmail.com>
2026-04-26 21:09:08 +0700

    done2

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def api_expert():
         return jsonify({'success': False, 'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5050)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
